bb5eb7442f095489cdce3a72c20c0af86a118726
Maksim Grekoff <mgrekov5@gmail.com>
2022-10-23 11:27:19 +0300

    fix

--- Favourite Movies/script.js
+++ Favourite Movies/script.js
@@ -29,5 +29,9 @@
 // // Sibling Node Traversal
 
 let ul = document.querySelector('ul');
+const div = document.querySelector('div');
 
-console.log(ul);
+console.log(div.childNodes);
+
+console.log(ul.previousElementSibling);
+console.log(ul.nextElementSibling);
